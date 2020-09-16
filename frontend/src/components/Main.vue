282ee423ebb5f7cb64bbb7c2be40536e2142ac00
caddyspoon <cwb5116@gmail.com>
2020-09-17 03:47:06 +0900

    S03P22A503-19 장고 API 프론트 테스트

---
+++ frontend/src/components/Main.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <h1>다음은 API위한 테스트 인풋 박스입니다.</h1>
+          <v-text-field
+            label="농수산물"
+            solo
+            v-model="TestInput"
+            @keypress.enter="TextInput"
+          >
+          </v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        TestInput: ''
+
+      }
+   },
+   methods:{
+     TextInput() {
+       console.log(this.TestInput)
+     }
+   }
+  }
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 
+// Global Font & Color css
+import "@/assets/font.css";
+import "@/assets/color.css"
+
 Vue.config.productionTip = false
 
 new Vue({
