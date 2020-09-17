fcc828c82f972f4895575773fcf277207cac2fed
홍두표 <hdp0545@gmail.com>
2020-09-17 12:53:34 +0900

    S03P22A503-20 프론트에 화면까지만 띄움 목록화면

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -3,13 +3,18 @@
     <v-row class="text-center">
       <v-col cols="12">
         <h1>다음은 API위한 테스트 인풋 박스입니다.</h1>
-          <v-text-field
-            label="농수산물"
-            solo
-            v-model="TestInput"
-            @keypress.enter="TextInput"
-          >
-          </v-text-field>
+        <v-text-field
+          label="농수산물"
+          solo
+          v-model="testInput"
+          @keypress.enter="textInput"
+        >
+        </v-text-field>
+        <recipe-card  
+          v-for="(recipe, index) in recipes" 
+          :key="index" 
+          :recipe="recipe">
+        </recipe-card> 
       </v-col>
     </v-row>
   </v-container>
@@ -17,24 +22,28 @@
 
 <script>
   // import http from "@/util/http-common"
-  import axios from 'axios';
+  import axios from 'axios'
+  import RecipeCard from '@/components/RecipeCard.vue'
   const SERVER_URL = 'http://localhost:8301'
 
 
   export default {
     name: 'HelloWorld',
+    components : {
+      RecipeCard,
+    },
     data() {
       return {
-        TestInput: ''
-
+        testInput: '',
+        recipes: [],
       }
    },
    methods:{
-     TextInput() {
-       console.log(this.TestInput)
-       axios.get(`${SERVER_URL}/api/recipe/grocery/${this.TestInput}`)
+     textInput() {
+       console.log(this.testInput)
+       axios.get(`${SERVER_URL}/api/recipe/grocery/${this.testInput}`)
        .then(response => {
-         console.log(response)
+        this.recipes = response.data
        })
 
      }
