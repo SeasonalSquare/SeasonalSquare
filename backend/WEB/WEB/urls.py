0f2b66eaec95a90506011e306bb0f5bbef5fee2a
hdp0545 <hdp0545@gmail.com>
2020-09-21 17:09:41 +0900

    Edit Url

--- backend/WEB/WEB/urls.py
+++ backend/WEB/WEB/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('api/accounts/', include('accounts.urls')),
     # 직접 만든 적은 없지만 rest_auth라는 앱이 생긴다.
     # 로그인 & 로그아웃
-    path('rest-auth/', include('rest_auth.urls')),
+    path('api/rest-auth/', include('rest_auth.urls')),
     # 회원가입
-    path('rest-auth/signup/', include('rest_auth.registration.urls'))
+    path('api/rest-auth/signup/', include('rest_auth.registration.urls'))
 ]
