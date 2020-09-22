2cef7b6245ebea2a54d254b08ce1ea32f74da6c0
이진석 <qlraor09@naver.com>
2020-09-22 13:03:38 +0900

    account_models

--- backend/WEB/accounts/models.py
+++ backend/WEB/accounts/models.py
@@ -2,6 +2,24 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 
+
+class Vegi(models.Model):
+    vegi_type = models.CharField(max_length=50)
+
+
+class Allergy(models.Model):
+    aller_type = models.TextField()
+
+
 class User(AbstractUser):
-    pass
-# Create your models here.
+    allergy = models.ManyToManyField(
+        Allergy,
+        blank=True, null=True,
+        related_name = 'Allergy',
+    )
+    vegi = models.ForeignKey(
+        Vegi,
+        on_delete=models.CASCADE,
+        blank=True, null=True
+        )
+
