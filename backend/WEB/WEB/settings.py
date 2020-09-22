5f5e09a08d7a64c2bc918fd9de5124bac5d32082
hdp0545 <hdp0545@gmail.com>
2020-09-22 17:54:19 +0900

    Add Docker Settings

--- backend/WEB/WEB/settings.py
+++ backend/WEB/WEB/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
@@ -98,8 +99,15 @@ WSGI_APPLICATION = 'WEB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'seasonal_square',
+        'USER': 'root',
+        'PASSWORD': 'ssafy',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
