c496436456ef80d5dfad3c5c9a71ac1b30388432
hdp0545 <hdp0545@gmail.com>
2020-10-05 17:10:47 +0900

    S03P23A503-53 | 알러지 비건 저장하기

--- backend/WEB/accounts/urls.py
+++ backend/WEB/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
+from . import views
 
 app_name = 'accounts'
 urlpatterns = [
     path('token/', obtain_jwt_token),
     path('token/verify/', verify_jwt_token),
     path('token/refresh/', refresh_jwt_token),
+    path('set-allergy/', views.set_allergy),
 ]
--- backend/WEB/accounts/views.py
+++ backend/WEB/accounts/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.response import Response
 # auth_login
 from django.contrib.auth import get_user_model
 from django.contrib.auth import login as auth_login
@@ -13,16 +14,39 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.views.decorators.http import require_POST
 
-from .forms import CustomUserCreationForm
+# django model
+from django.shortcuts import render, get_object_or_404
+from .models import Allergy, Vegetarian
 
-from .models import Post
+# from .forms import CustomUserCreationForm
+# from .models import Post
 
 
-@api_view(['GET'])
+# @api_view(['GET'])
+# @permission_classes((IsAuthenticated, ))
+# @authentication_classes((JSONWebTokenAuthentication,))
+# def posts(request):
+#     posts = Post.objects.filter(
+#         published_at__isnull=False).order_by('-published_at')
+#     post_list = serializers.serialize('json', posts)
+#     return HttpResponse(post_list, content_type="text/json-comment-filtered")
+
+
+@api_view(['POST'])
 @permission_classes((IsAuthenticated, ))
 @authentication_classes((JSONWebTokenAuthentication,))
-def posts(request):
-    posts = Post.objects.filter(
-        published_at__isnull=False).order_by('-published_at')
-    post_list = serializers.serialize('json', posts)
-    return HttpResponse(post_list, content_type="text/json-comment-filtered")
+def set_allergy(request):
+    print(request.data)
+    user = request.user
+    allergy_list = request.data['allergy_list']
+    for a_type in allergy_list:
+        allergy_model = get_object_or_404(Allergy, a_type=a_type)
+        user.allergy.add(allergy_model)
+    vegan_list = request.data['vegan_list']
+    for v_type in vegan_list:
+        vegi_model = get_object_or_404(Vegetarian, v_type=v_type)
+        user.vegetarian.add(vegi_model)
+    return Response(True)
+
+
+
